44e761ef358eef4cb3b5e856dbfeaae1926bd12d
Shashi Shankar <shankarshashi386@gmail.com>
2025-06-14 21:59:10 +0530

    delete node added

--- AvlTree.cpp
+++ AvlTree.cpp
@@ -103,6 +103,93 @@ Node* insert(Node* root, int key) {
     return root;
 }
 
+Node* deleteNode(Node* root, int key)
+{
+    //add code here,
+    if(!root)
+    return NULL;
+    
+    if(key<root->data) // left side
+    {
+        root->left = deleteNode(root->left, key);
+    }
+    else if(key>root->data)  // right side
+    {
+        root->right = deleteNode(root->right, key);
+    }
+    else
+    {
+        // Leaf node
+        if(!root->left && !root->right)
+        {
+            delete root;
+            return NULL;
+        }
+        // only one child exist
+        else if(root->left && !root->right) // Left child
+        {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        else if(!root->left && root->right) // Right child
+        {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        // Both child exist
+        else
+        {
+            // Right side smallest element
+            Node* curr = root->right;
+            while(curr->left)
+            {
+                curr = curr->left;
+            }
+            
+            root->data = curr->data;
+            root->right = deleteNode(root->right, curr->data);
+        }
+    }
+    
+    // Update the height
+    root->height = 1+max(getheight(root->left), getheight(root->right));
+    
+    //Check the balance
+    int balance = getbalance(root);
+    
+    // Left side
+    if(balance > 1)
+    {
+        // LL
+        if(root->left && getbalance(root->left) >= 0)
+        return rightRotate(root);
+        // LR
+        else
+        {
+            root->left = leftRotate(root->left);
+            return rightRotate(root);
+        }
+    }
+    // Right side
+    if(balance < -1)
+    {
+        // RR
+        if(root->right && getbalance(root->right) <= 0)
+        return leftRotate(root);
+        // RL
+        else 
+        {
+            root->right = rightRotate(root->right);
+            return leftRotate(root);
+        }
+    }
+    else 
+    return root;
+    
+}
+
 void preorder(Node* root) {
     if(root == nullptr) return;
     
