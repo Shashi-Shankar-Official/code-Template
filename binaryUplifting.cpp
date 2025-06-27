eae329c443c6cd2975c2e0bbd8e05428cb51cc0d
Shashi Shankar <shankarshashi386@gmail.com>
2025-06-27 09:06:00 +0530

    tries and binary uplifting completed

--- binaryUplifting.cpp
+++ binaryUplifting.cpp
@@ -1,120 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int LOG = 20; // log2(N) where N <= 1e5
+const int N= 2e5+1, M=20;
+
+vector<int> adj[N];
+int depth[N], parent[N][M];
+
+void dfs(int curr, int par) {
+    depth[curr] = depth[par] + 1;
+    parent[curr][0] = par;
+    for(int j=1;j<M;j++) {
+        parent[curr][j] = parent[parent[curr][j-1]][j-1];
+    }
+
+    for(auto x:adj[curr]) {
+        if(x != par) {
+            dfs(x, curr);
+        }
+    }
+}
+
+int kthParent(int u, int k) {
+    for(int i=M-1; i>=0; i--) {
+        if((1 << i) & k) {
+            u=parent[u][i];
+        }
+    }
+
+    return u;
+}
+
+
+
+//LCA- Lowest common ancestor
+int LCA(int u, int v) {
+    if(u==v) return u;
+    if(depth[u] < depth[v]) {
+        swap(u,v);
+    }
+    int diff = depth[u] - depth[v];
+    for(int j= M-1; j>=0; j--) {
+        if((diff >> j) & 1) {
+            u = parent[u][j];
+        }
+    }
+
+    for(int j= M-1; j>=0; j--) {
+        if(parent[u][j] != parent[v][j]) {
+            u = parent[u][j];
+            v = parent[v][j];
+        }
+    }
+
+    if(u == v) {
+        return u;
+    } else {
+        return parent[u][0];
+    }
+}
 
 int main() {
-    int n;
-    cin >> n;
+    int n,q;
+    cin>>n>>q;
 
-    // Node values (1-indexed)
-    vector<int> nodeValue(n + 1);
-    for (int i = 1; i <= n; i++) {
-        cin >> nodeValue[i];
+    for(int i=2;i<=n;i++) {
+        int x;
+        cin>>x;
+        adj[x].push_back(i);
     }
+    dfs(1,0);
 
-    // Adjacency list of the tree
-    vector<vector<int>> tree(n + 1);
-    for (int i = 0; i < n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        tree[u].push_back(v);
-        tree[v].push_back(u);
-    }
-
-    // Binary lifting tables
-    vector<vector<int>> parent(n + 1, vector<int>(LOG)); // parent[node][j] = 2^j-th ancestor
-    vector<vector<int>> maxOnPath(n + 1, vector<int>(LOG)); // max value from node to its 2^j-th ancestor
-    vector<int> level(n + 1); // depth level of each node
-
-    // DFS to fill parent[node][0] and maxOnPath[node][0]
-    function<void(int, int)> dfs = [&](int current, int par) {
-        level[current] = level[par] + 1;
-        parent[current][0] = par;
-        maxOnPath[current][0] = nodeValue[current];
-
-        for (int neighbor : tree[current]) {
-            if (neighbor != par) {
-                dfs(neighbor, current);
-            }
-        }
-    };
-
-    dfs(1, 0); // Assuming node 1 is the root
-
-    // Precompute binary lifting tables
-    for (int j = 1; j < LOG; j++) {
-        for (int i = 1; i <= n; i++) {
-            int midAncestor = parent[i][j - 1];
-            parent[i][j] = parent[midAncestor][j - 1];
-            maxOnPath[i][j] = max(maxOnPath[i][j - 1], maxOnPath[midAncestor][j - 1]);
+    for(int i =0; i<q;i++) {
+        int u,k;
+        cin>>u>>k;
+        int parent = kthParent(u,k);
+        if(parent == 0) {
+            cout<<"-1\n";
+        } else {
+            cout<<parent<<"\n";
         }
     }
-
-    // Lifts node u up by k levels
-    auto liftNode = [&](int u, int k) {
-        for (int j = 0; j < LOG; j++) {
-            if ((k >> j) & 1) {
-                u = parent[u][j];
-            }
-        }
-        return u;
-    };
-
-    // Gets the maximum value on the path from node u up by k levels
-    auto getMaxAlongPath = [&](int u, int k) {
-        int maxVal = 0;
-        for (int j = 0; j < LOG; j++) {
-            if ((k >> j) & 1) {
-                maxVal = max(maxVal, maxOnPath[u][j]);
-                u = parent[u][j];
-            }
-        }
-        return maxVal;
-    };
-
-    // Computes the Lowest Common Ancestor (LCA) of u and v
-    auto getLCA = [&](int u, int v) {
-        if (level[u] < level[v]) swap(u, v);
-        u = liftNode(u, level[u] - level[v]);
-
-        if (u == v) return u;
-
-        for (int j = LOG - 1; j >= 0; j--) {
-            if (parent[u][j] != parent[v][j]) {
-                u = parent[u][j];
-                v = parent[v][j];
-            }
-        }
-        return parent[u][0];
-    };
-
-    // Distance between two nodes u and v
-    auto getDistance = [&](int u, int v) {
-        int lca = getLCA(u, v);
-        return level[u] + level[v] - 2 * level[lca];
-    };
-
-    // Maximum value on the path from u to v
-    auto getMaxBetweenNodes = [&](int u, int v) {
-        int lca = getLCA(u, v);
-        int maxU = getMaxAlongPath(u, level[u] - level[lca]);
-        int maxV = getMaxAlongPath(v, level[v] - level[lca]);
-        return max({maxU, maxV, nodeValue[lca]});
-    };
-
-    // Process queries
-    int q;
-    cin >> q;
-
-    while (q--) {
-        int u, v;
-        cin >> u >> v;
-
-        // Example usage:
-        // cout << "Distance: " << getDistance(u, v) << '\n';
-        // cout << "Max value on path: " << getMaxBetweenNodes(u, v) << '\n';
-    }
-
     return 0;
 }
